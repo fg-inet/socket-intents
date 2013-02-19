9bfd2a6cc092a38e7e38dcd990a2cfbe79487efa
Theresa Enghardt <theresa@net.t-labs.tu-berlin.de>
2013-02-19 11:44:55 +0100

    Fixed documentation of data structures (was broken in Doxygen)

--- clib/muacc_ctx.h
+++ clib/muacc_ctx.h
@@ -16,34 +16,34 @@
 
 /** Linked list of socket options */
 typedef struct socketopt {
-	int 				level;				/**> Level at which the socket option is valid */
-	int 				optname;			/**> Identifier of the option */
-	void 				*optval;			/**> Pointer to the value */
-	socklen_t 			optlen;				/**> Length of the value */
-	struct socketopt 	*next;				/**> Pointer to the next socket option */
+	int 				level;				/**< Level at which the socket option is valid */
+	int 				optname;			/**< Identifier of the option */
+	void 				*optval;			/**< Pointer to the value */
+	socklen_t 			optlen;				/**< Length of the value */
+	struct socketopt 	*next;				/**< Pointer to the next socket option */
 } socketopt_t;
 
 /** Internal muacc context struct */
 struct _muacc_ctx {
-	int usage;                          	/**> reference counter */
-	uint8_t locks;                      	/**> lock to avoid multiple concurrent requests to MAM */
-	int mamsock;                        	/**> socket to talk to/in MAM */
-	struct evbuffer *out;					/**> output buffer when used with libevent2 */
-	struct evbuffer *in;					/**> input buffer when used with libevent2 */
-	muacc_mam_action_t state;				/**> state machine state */
+	int usage;                          	/**< reference counter */
+	uint8_t locks;                      	/**< lock to avoid multiple concurrent requests to MAM */
+	int mamsock;                        	/**< socket to talk to/in MAM */
+	struct evbuffer *out;					/**< output buffer when used with libevent2 */
+	struct evbuffer *in;					/**< input buffer when used with libevent2 */
+	muacc_mam_action_t state;				/**< state machine state */
 	/* fields below will be serialized */
-	struct sockaddr *bind_sa_req;       	/**> local address requested */
-	socklen_t 		 bind_sa_req_len;      	/**> length of bind_sa_req*/
-	struct sockaddr *bind_sa_res;       	/**> local address choosen by MAM */
-	socklen_t 		 bind_sa_res_len;      	/**> length of bind_sa_res*/
-	struct sockaddr *remote_sa_req;     	/**> remote address requested */
-	socklen_t 		 remote_sa_req_len;    	/**> length of remote_sa_req*/
-	char 			*remote_hostname;      	/**> hostname to resolve */
-	struct addrinfo	*remote_addrinfo_hint;	/**> hints for resolving */
-	struct addrinfo	*remote_addrinfo_res;	/**> candidate remote addresses (sorted by MAM preference) */
-	struct sockaddr *remote_sa_res;     	/**> remote address choosen in the end */
-	socklen_t 		 remote_sa_res_len;    	/**> length of remote_sa_res */
-	socketopt_t		*socket_options;		/**> associated socket options */
+	struct sockaddr *bind_sa_req;       	/**< local address requested */
+	socklen_t 		 bind_sa_req_len;      	/**< length of bind_sa_req*/
+	struct sockaddr *bind_sa_res;       	/**< local address choosen by MAM */
+	socklen_t 		 bind_sa_res_len;      	/**< length of bind_sa_res*/
+	struct sockaddr *remote_sa_req;     	/**< remote address requested */
+	socklen_t 		 remote_sa_req_len;    	/**< length of remote_sa_req*/
+	char 			*remote_hostname;      	/**< hostname to resolve */
+	struct addrinfo	*remote_addrinfo_hint;	/**< hints for resolving */
+	struct addrinfo	*remote_addrinfo_res;	/**< candidate remote addresses (sorted by MAM preference) */
+	struct sockaddr *remote_sa_res;     	/**< remote address choosen in the end */
+	socklen_t 		 remote_sa_res_len;    	/**< length of remote_sa_res */
+	socketopt_t		*socket_options;		/**< associated socket options */
 };
 
 /** Helper to allocate and initalize _muacc_ctx
@@ -83,10 +83,10 @@ int _unlock_ctx (struct _muacc_ctx *_ctx);
  *
  */
 size_t _muacc_pack_ctx(
-	char *buf,						/**> [in]		buffer to write TLVs to */
-	size_t *pos,					/**> [in,out]	position within buf */
-	size_t len,						/**> [in]		length of buf	*/
-	const struct _muacc_ctx *ctx	/**> [in]		context to pack */
+	char *buf,						/**< [in]		buffer to write TLVs to */
+	size_t *pos,					/**< [in,out]	position within buf */
+	size_t len,						/**< [in]		length of buf	*/
+	const struct _muacc_ctx *ctx	/**< [in]		context to pack */
 );
 
 /** parse a single TLV and push its content to the respective member of _muacc_ctx
@@ -95,10 +95,10 @@ size_t _muacc_pack_ctx(
  * has to keep memory consistent (free stuff changed/overwritten)
  */
 int _muacc_unpack_ctx(
-	muacc_tlv_t tag,				/**> [in]		tag of the TLV */
-	const void *data,				/**> [in]		value of the TLV */
-	size_t data_len,				/**> [in]		length of the TLV */
-	struct _muacc_ctx *_ctx			/**> [in]		context to put parsed data in */
+	muacc_tlv_t tag,				/**< [in]		tag of the TLV */
+	const void *data,				/**< [in]		value of the TLV */
+	size_t data_len,				/**< [in]		length of the TLV */
+	struct _muacc_ctx *_ctx			/**< [in]		context to put parsed data in */
 );
 
 /* helper to print ctx
