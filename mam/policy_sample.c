52cdf2ffcbeae081a97a70a14647928b1997642c
Theresa Enghardt <theresa@net.t-labs.tu-berlin.de>
2013-04-03 14:44:47 +0200

    Adjusted policy sample to the new interface (i.e. passing contexts)

--- mam/policy_sample.c
+++ mam/policy_sample.c
@@ -3,21 +3,25 @@
 #define on_connect_request policy_sample_LTX_on_connect_request
 
 #include <stdio.h>
+#include "mam.h"
 
-int init()
+int init(mam_context_t *mctx)
 {
 	printf("Policy sample library has been loaded.\n");
+	mam_print_context(mctx);
 	return 0;
 }
 
-int on_resolve_request()
+int on_resolve_request(request_context_t *rctx)
 {
 	printf("Resolve request handled by policy sample library\n");
+	mam_print_request_context(rctx);
 	return 0;
 }
 
-int on_connect_request()
+int on_connect_request(request_context_t *rctx)
 {
 	printf("Connect request handled by policy sample library\n");
+	mam_print_request_context(rctx);
 	return 0;
 }
