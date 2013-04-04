6e5469ebc3e89be8491dd14e2bf78a242599fdd8
Theresa Enghardt <theresa@net.t-labs.tu-berlin.de>
2013-04-04 13:25:54 +0200

    Adjusted policy_sample to the new callback function interface, passing event_base and sending response

--- mam/policy_sample.c
+++ mam/policy_sample.c
@@ -4,6 +4,11 @@
 
 #include <stdio.h>
 #include "mam.h"
+#include <event2/event.h>
+
+#include "../clib/muacc.h"
+#include "../clib/muacc_types.h"
+#include "../clib/muacc_tlv.h"
 
 int init(mam_context_t *mctx)
 {
@@ -12,16 +17,18 @@ int init(mam_context_t *mctx)
 	return 0;
 }
 
-int on_resolve_request(request_context_t *rctx)
+int on_resolve_request(request_context_t *rctx, struct event_base *base)
 {
 	printf("Resolve request handled by policy sample library\n");
 	mam_print_request_context(rctx);
+	_muacc_send_ctx_event(rctx, muacc_act_getaddrinfo_resolve_resp);
 	return 0;
 }
 
-int on_connect_request(request_context_t *rctx)
+int on_connect_request(request_context_t *rctx, struct event_base *base)
 {
 	printf("Connect request handled by policy sample library\n");
 	mam_print_request_context(rctx);
+	_muacc_send_ctx_event(rctx, muacc_act_connect_resp);
 	return 0;
 }
