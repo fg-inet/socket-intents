e632138ff0dfa601bfe48afb9d43e2e2bed26f8a
Alexander Couzens <lynxis@fe80.eu>
2013-12-03 07:41:42 +0100

    FIXUP: reorganize include of policy.h

--- policies/policy.h
+++ policies/policy.h
@@ -4,7 +4,7 @@
 #include <event2/event.h>
 #include <arpa/inet.h>
 
-#include "../lib/muacc.h"
-#include "../lib/muacc_util.h"
-#include "../lib/intents.h"
-#include "../mam/mam.h"
+#include "lib/muacc.h"
+#include "lib/muacc_util.h"
+#include "lib/intents.h"
+#include "mam/mam.h"
