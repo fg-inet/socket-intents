9314e11f3933d21ffb214cc387e9f15885d71296
Alexander Couzens <lynxis@fe80.eu>
2013-12-08 18:17:15 +0100

    muacsocks: sort includes of socksd

--- muacsocks/muacsocksd.c
+++ muacsocks/muacsocksd.c
@@ -1,14 +1,14 @@
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+#include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
-#include <assert.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "clib/muacc_client.h"
 
